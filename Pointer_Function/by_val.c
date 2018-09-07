7937b1fb76aa04ffedea2651a39c890b80e778ad
REPIDUS <moondj123@naver.com>
2018-09-07 17:52:48 +0900

    practice the rest of chapter "Understanding pointer and function."

---
+++ Pointer_Function/by_val.c
@@ -0,0 +1,16 @@
+/* by_val.c */
+#include <stdio.h>
+
+int add(int a, int b);
+
+int main(void) {
+    int val1 = 10;
+    int val2 = 20;
+    printf("Result: %d", add(val1, val2));
+
+    return 0;
+}
+
+int add(int a, int b) {
+    return a+b;
+}
