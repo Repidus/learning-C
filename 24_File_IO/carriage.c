5ef516d66286bfebb43f63a603be4d0751e59696
REPIDUS <moondj123@naver.com>
2019-04-04 21:46:58 +0900

    24장 '파일 입출력' 실습

---
+++ 24_File_IO/carriage.c
@@ -0,0 +1,9 @@
+/* carriage.c */
+#include <stdio.h>
+
+int main() {
+	printf("ABCDEFG \r");
+	printf("1234567   ");
+
+	return 0;
+}
---
+++ 24_File_IO/file_basic.c
@@ -0,0 +1,22 @@
+/* file_basic.c */
+#include <stdio.h>
+
+int main(void) {
+	int state;
+
+	/* 파일의 개방 */
+	FILE *file = fopen("Test.txt", "wt");
+	if (file == NULL) {
+		printf("file open error!\n");
+		return 1;
+	}
+
+	/* 파일의 종결 */
+	state = fclose(file);
+	if (state != 0) {
+		printf("file close error!\n");
+		return 1;
+	}
+
+	return 0;
+}
